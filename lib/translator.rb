aa0a815805aad1091664393137f5df792d9f0dd4
Aidan K McBride <aidankmcbride@gmail.com>
2020-02-24 22:53:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(file_path)
   newHash = {"get_meaning": {}, "get_emoticon": {} } 
   lib.each do |meaning, emoticons| 
     newHash["get_meaning"][emoticons[1]] = meaning
-    newHash["get_emoticon"][]
+    newHash["get_emoticon"][emoticons[0]] = emoticons[1]
   newHash
 end
 
