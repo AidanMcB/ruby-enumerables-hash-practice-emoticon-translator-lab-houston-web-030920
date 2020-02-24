5afcb70e2b6a15613d44ba0070e1b1b6f62a40d6
Aidan K McBride <aidankmcbride@gmail.com>
2020-02-24 22:51:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@
 require "yaml"
 def load_library(file_path)
   lib = YAML.load_file(file_path)
-  newHash = {:get_meaning => {}, :get_emoticon => {} } 
+  newHash = {"get_meaning": {}, "get_emoticon": {} } 
   }
   newHash
 end
